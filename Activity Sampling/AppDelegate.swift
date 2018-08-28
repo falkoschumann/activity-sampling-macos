3a9c55da09b3c0abb928ffab25bcbde238c04fb2
Falko Schumann <falko.schumann@muspellheim.de>
2018-08-28 17:00:25 +0200

    Initial Commit

---
+++ Activity Sampling/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Activity Sampling
+//
+//  Created by Falko Schumann on 28.08.18.
+//  Copyright © 2018 Falko Schumann. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Activity Sampling/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Activity Sampling
+//
+//  Created by Falko Schumann on 28.08.18.
+//  Copyright © 2018 Falko Schumann. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
