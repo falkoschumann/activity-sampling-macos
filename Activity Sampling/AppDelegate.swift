f6f3aa132d0171cf9314c818775854531a1f2349
Falko Schumann <falko.schumann@muspellheim.de>
2018-11-19 22:37:56 +0100

    Clean up app

--- Activity Sampling/AppDelegate.swift
+++ Activity Sampling/AppDelegate.swift
@@ -9,47 +9,10 @@
 import Cocoa
 
 @NSApplicationMain
-class AppDelegate: NSObject, NSApplicationDelegate, ClockDelegate {
-    
-    private let clock = Clock()
-    private let period = Period()
-    
-    func applicationDidFinishLaunching(_ aNotification: Notification) {
-        App.shared.start()
-    }
-    
-    func applicationWillTerminate(_ aNotification: Notification) {
-        App.shared.stop()
-    }
+class AppDelegate: NSObject, NSApplicationDelegate {
     
     func applicationShouldTerminateAfterLastWindowClosed(_ sender: NSApplication) -> Bool {
         return true
-    }
-    
-    // MARK: Clock Delegate
-    
-    func clockTicked(_ clock: Clock, currentTime: Date) {
-        period.check(currentTime)
-    }
-    
-    // MARK: Period Delegate
-    
-    func periodStarted(_ period: Period, duration: TimeInterval) {
-        NotificationCenter.default.post(name: Notification.Name.init(rawValue: "PeriodStarted"),
-                                        object: period,
-                                        userInfo: ["duration": duration])
-    }
-    
-    func periodProgressed(_ period: Period, elapsedTime: TimeInterval, remainingTime: TimeInterval) {
-        NotificationCenter.default.post(name: Notification.Name.init(rawValue: "PeriodProgressed"),
-                                        object: period,
-                                        userInfo: ["elapsedTime": elapsedTime, "remainingTime": remainingTime])
-    }
-    
-    func periodEnded(_ period: Period, timestamp: Date) {
-        NotificationCenter.default.post(name: Notification.Name.init(rawValue: "PeriodEnded"),
-                                        object: period,
-                                        userInfo: ["timestamp": timestamp])
     }
     
 }
